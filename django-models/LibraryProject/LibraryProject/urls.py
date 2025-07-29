bfbabc33235937dd62fb68aa4376794351f18956
allan_Ben <allanbenjuma@gmail.com>
2025-07-29 13:15:51 +0300

    Added authentication functionality

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -17,7 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.contrib.auth.views import LoginView, LogoutView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('relationship_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),  # built-in auth views
+    
+    
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,14 @@ from .models import Library
 
 from django.views.generic.detail import DetailView
 
+from django.contrib import messages
+from django.shortcuts import render, redirect
+
+from django.views.generic import CreateView
+from django.contrib.auth import login
+
+from django.contrib.auth.forms import UserCreationForm
+
 # Create your views here.
 
 def list_books(request):
@@ -19,3 +27,16 @@ class LibraryDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         context['books'] = self.object.books.all()
         return context
+
+def register_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Account created successfully. You can now log in.')
+            return redirect('login')
+    else:
+        form = UserCreationForm() # built-in form for user registration
+    return render(request, 'relationship_app/register.html', {'form': form})
+
+
