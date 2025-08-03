b0ffd39be587d52333f6fa71faa26f86c2f11902
allan_Ben <allanbenjuma@gmail.com>
2025-08-03 13:51:35 +0300

    Added advanced features and security

--- advanced_features_and_security/LibraryProject/relationship_app/models.py
+++ advanced_features_and_security/LibraryProject/relationship_app/models.py
@@ -70,8 +70,8 @@ class CustomUserManager(BaseUserManager):
 
 
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(null=True, blank=True)
+    date_of_birth= models.DateField(null=True, blank=True)
+    profile_photo= models.ImageField(null=True, blank=True)
     phone_number = models.CharField(max_length=20, blank=True)
     is_verified = models.BooleanField(default=False)
 
