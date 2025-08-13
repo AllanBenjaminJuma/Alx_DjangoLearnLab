378d2f3e8f737e7e7a552aa8a2940c4278fadb6b
allan_Ben <allanbenjuma@gmail.com>
2025-08-13 14:12:29 +0300

    Advance API Project

---
+++ advanced-api-project/api/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+import datetime
+
+"""
+    Represents an Author.
+    
+"""
+class Author(models.Model):
+    name = models.CharField(max_length=200)
+
+"""
+    Represents a Book.
+    Fields:
+        title(str): The title of the book.
+        publication_year(int): The publication year of the book.
+        author(str): The author of the book 
+    
+"""
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = datetime.datetime.year
+    author = models.ManyToManyField(Author)
---
+++ advanced-api-project/api/serializers.py
@@ -0,0 +1,28 @@
+from rest_framework import serializers
+from api.models import Book
+import datetime
+
+"""
+Serializer for the book model.
+Adds:
+        - A validation for the publication year not being greater than the current year.
+"""
+
+class BookSerializer(serializers.Serializer):
+    title = serializers.CharField(required = True, allow_blank = True, max_length = 200)
+    publication_year = serializers.IntegerField(required = True, max_length = 4, default=2025)
+    author = serializers.CharField(required = True, max_length = 200)
+    
+    
+    def validate_pub_year(self, value):
+        if value > datetime:
+            raise serializers.ValidationError("Publication year cannot be greater than current Year.")
+        return value
+
+"""
+    Serializer for the Author Model.    
+"""
+
+class AuthorSerializer(serializers.Serializer):
+    name = serializers.CharField(required = True, max_length = 200)
+    
