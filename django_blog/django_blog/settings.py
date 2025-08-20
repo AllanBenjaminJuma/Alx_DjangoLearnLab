200821ff37a9554834ff0f341f448610ed86cd9d
allan_Ben <allanbenjuma@gmail.com>
2025-08-20 11:01:01 +0300

    Added the new postgresql database configuration

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,7 +77,16 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    
+    'postgres':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_blog',
+        'USER': 'allan',
+        'PASSWORD': 'allan',
+        'HOST': 'localhost',
+        'PORT': '5432',  
+    },
 }
 
 
